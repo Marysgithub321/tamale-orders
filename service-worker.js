d5bb2dfa05221e5b793f5982e0428ccde5ea2cbb
Marysgithub321 <peters_maria@hotmail.com>
2024-06-20 11:40:49 -0600

    fixed service worker

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,43 @@
+// Define a unique cache name
 const CACHE_NAME = "tamale-orders-cache-v1";
+
+// List of resources to be cached
 const urlsToCache = [
   "/",
   "/index.html",
   "/styles.css",
   "/main.js",
   "/manifest.json",
-  "/favicon-32x32.png",
+  "/images/favicon-32x32.png",
+  "/images/icon-192x192.png",
+  "/images/icon-512x512.png",
 ];
 
-self.addEventListener("install", function (event) {
+// Install event: Add resources to cache
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache
+        .addAll(urlsToCache)
+        .then(() => console.log("Resources added to cache"))
+        .catch((error) =>
+          console.error("Failed to add resources to cache", error)
+        );
     })
   );
 });
 
-self.addEventListener("fetch", function (event) {
+// Fetch event: Serve cached resources or fetch from network
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      // If resource is found in cache, return it
+      if (response) {
+        return response;
+      }
+      // Otherwise, fetch the resource from the network
+      return fetch(event.request);
     })
   );
 });
